097e3f57850ac00370108b1fdf465f041b2d9dc6
Annetkhavere <anneywan5@gmail.com>
2022-04-10 19:42:15 +0300

    add quote-item component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { ButtonComponent } from './components/button/button.component';
+import { QuotesComponent } from './components/quotes/quotes.component';
+import { QuoteItemComponent } from './components/quote-item/quote-item.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    ButtonComponent
+    ButtonComponent,
+    QuotesComponent,
+    QuoteItemComponent
   ],
   imports: [
     BrowserModule,
