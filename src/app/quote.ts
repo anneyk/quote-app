034365bf87adc4f3d85f75c2110e33466e41a845
Annetkhavere <anneywan5@gmail.com>
2022-04-11 10:05:35 +0300

    move files

--- src/app/quote.ts
+++ src/app/quote.ts
@@ -1,7 +1,11 @@
-export interface Quote {
-  quote: string;
-  author: string;
-  dateSubmitted: string;
-  submittedBy: string;
+export class Quote {
+
+  showQuoteDetails: boolean;
+
+  constructor(public id: string, public quote: string, public author: string,
+      public upvotes: number,public downvotes: number, public submitted_by: string,
+      public created_at: Date, public isFavorite: boolean){
+      this.showQuoteDetails = false;
+  }
 
 }
